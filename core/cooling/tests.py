Correct computation of film from hole at zero point. Add ability to set distribution of cool air mass rate density. Add ability to set distribution of gas temperature. Add ability to save required for cooling results of profiling.

core/cooling/tests.py
@@ -1,9 +1,10 @@
 from .convective_defl import SectorCooler, BladeSection, GasBladeHeatExchange
-from .tools import DeflectorAverageParamCalculator, FilmCalculator
-from .film_defl import FilmSectorCooler, FilmBladeCooler
+from .tools import DeflectorAverageParamCalculator, FilmCalculator, get_theta, get_T_gas, get_g_cool
+from .film_defl import FilmSectorCooler, FilmBladeCooler, get_sa_cooler
 from gas_turbine_cycle.gases import Air, KeroseneCombustionProducts, NaturalGasCombustionProducts
 import unittest
 import numpy as np
+from scipy.integrate import quad
 from scipy.interpolate import interp1d
 from ..average_streamline.turbine import TurbineType, Turbine
 from ..profiling.turbine import TurbineProfiler, ProfilingType
@@ -58,7 +59,7 @@ class BladeSectorTest(unittest.TestCase):
                                                          self.conv_cooler.cool_fluid
                                                          )
 
-        self.x_hole1 = [-0.06, -0.04, -0.015, 0.008, 0.015, 0.04]
+        self.x_hole1 = [-0.06, -0.04, -0.015, 0, 0.008, 0.015, 0.04]
         self.film1 = FilmCalculator(x_hole=self.x_hole1,
                                     hole_num=[15 for _ in self.x_hole1],
                                     d_hole=[0.001 for _ in self.x_hole1],
@@ -135,9 +136,6 @@ class BladeSectorTest(unittest.TestCase):
         self.assertAlmostEqual(Q1, Q2, places=7)
         self.assertAlmostEqual(Q2, Q3, places=7)
 
-    # def test_channel_width_plot(self):
-    #     self.cooler.plot_channel_width_plot(np.linspace(0.05, 0.25, 10))
-
     def test_local_params(self):
         """Проверка решения дифференциального уравнения."""
         cool_fluid = self.conv_cooler.cool_fluid
@@ -178,7 +176,7 @@ class BladeSectorTest(unittest.TestCase):
         self.conv_cooler.local_param.plot_T_wall()
         self.conv_cooler.local_param.plot_all()
 
-    def test_film_calculator_with_six_holes_rows(self):
+    def test_film_calculator_with_seven_holes_rows(self):
         self.film1.compute()
         x_arr = np.linspace(self.x_hole1[0] - 0.06, self.x_hole1[len(self.x_hole1) - 1] + 0.06, 400)
         self.film1.plot_film_eff(0, x_arr, create_fig=True, show=False)
@@ -186,11 +184,22 @@ class BladeSectorTest(unittest.TestCase):
         self.film1.plot_film_eff(2, x_arr, show=False)
         self.film1.plot_film_eff(3, x_arr, show=False)
         self.film1.plot_film_eff(4, x_arr, show=False)
-        self.film1.plot_film_eff(5, x_arr, show=True)
+        self.film1.plot_film_eff(5, x_arr, show=False)
+        self.film1.plot_film_eff(6, x_arr, show=False)
+        self.film1.plot_film_eff(7, x_arr, show=True)
 
         self.film1.plot_G_cool(x_arr)
         self.film1.plot_T_film(x_arr)
         self.film1.plot_alpha_film(x_arr, (4000, 8000))
+
+        self.assertAlmostEqual(abs(self.film1.get_G_cool(0) - self.film1.get_G_cool(self.x_hole1[3] - 1e-4) -
+                                   self.film1.dG_cool_hole[3]) / self.film1.dG_cool_hole[4], 0, places=4)
+        self.assertAlmostEqual(abs(self.film1.get_G_cool(0) - self.film1.get_G_cool(self.x_hole1[3] + 1e-4) -
+                               self.film1.dG_cool_hole[4]) / self.film1.dG_cool_hole[4], 0, places=4)
+        self.assertEqual(self.film1.G_cool0, self.film1.get_G_cool(0))
+        self.assertEqual(self.film1.hole_num[3], self.film1.hole_num[2] / 2)
+        self.assertEqual(self.film1.hole_num[4], self.film1.hole_num[5] / 2)
+        self.assertEqual(self.film1.hole_num[4], self.film1.hole_num[3])
 
     def test_film_calculator_with_two_holes_rows(self):
         self.film2.compute()
@@ -270,6 +279,11 @@ class DeflectorBladeFilmCoolingTest(unittest.TestCase):
         lam_blade_int = interp1d(T_wall_arr, lam_blade_arr, bounds_error=False, fill_value='extrapolate')
         x_hole_rel = [-0.8, -0.5, -0.15, 0.1, 0.35, 0.6]
 
+        self.theta = get_theta(0.5 * stage_prof.D1_in, 0.5 * stage_prof.D1_out, 0.5, 1.12)
+        self.T_gas_stag = get_T_gas(self.theta, 650, self.turbine[0].T0_stag)
+        self.G_cool0 = 0.2
+        self.g_cool0 = get_g_cool(self.theta, 2 * self.G_cool0 / (stage_prof.D1_out - stage_prof.D1_in))
+
         self.film_blade = FilmBladeCooler(sections=stage_prof.sa_sections,
                                           channel_width=0.001,
                                           wall_thickness=0.001,
@@ -282,31 +296,44 @@ class DeflectorBladeFilmCoolingTest(unittest.TestCase):
                                           d_hole=[0.5e-3 for _ in x_hole_rel],
                                           phi_hole=[0.9 for _ in x_hole_rel],
                                           mu_hole=[0.86 for _ in x_hole_rel],
-                                          T_gas_stag=stage_prof.T0_stag,
+                                          T_gas_stag=self.T_gas_stag,
                                           p_gas_stag=stage_prof.p0_stag,
-                                          G_gas=self.turbine.G_turbine,
                                           c_p_gas_av=stage_prof.c_p,
                                           lam_gas_in=stage_prof.lam_c0,
                                           lam_gas_out=stage_prof.lam_c1,
                                           work_fluid=type(self.turbine.work_fluid)(),
                                           T_cool0=650,
                                           p_cool_stag0=self.turbine.p_g_stag + 20e3,
-                                          G_cool0=0.2,
+                                          g_cool0=self.g_cool0,
                                           cool_fluid=Air())
 
     def test_partition(self):
-        self.assertEqual(sum(self.film_blade.G_gas_arr), self.film_blade.G_gas)
-        self.assertEqual(self.film_blade.G_gas_arr[0] * 2, self.film_blade.G_gas_arr[1])
-        self.assertEqual(self.film_blade.G_gas_arr[2] * 2, self.film_blade.G_gas_arr[1])
-        self.assertEqual(sum(self.film_blade.G_cool0_arr), self.film_blade.G_cool0)
-        self.assertEqual(self.film_blade.G_cool0_arr[0] * 2, self.film_blade.G_cool0_arr[1])
-        self.assertEqual(self.film_blade.G_cool0_arr[2] * 2, self.film_blade.G_cool0_arr[1])
+        self.film_blade.plot_T_gas_stag()
+        self.film_blade.plot_g_cool()
+        self.film_blade.plot_g_gas()
+        self.film_blade.plot_lam_gas_out()
+
+        self.assertAlmostEqual(abs(sum(self.film_blade.G_gas_arr) - self.film_blade.G_gas) / self.film_blade.G_gas,
+                               0, places=3)
+        self.assertAlmostEqual(abs(sum(self.film_blade.G_cool0_arr) - self.film_blade.G_cool0) / self.film_blade.G_cool0,
+                               0, places=3)
+
+        stage_prof = self.turbine_profiler[0]
+        T_gas_av1 = 2 * quad(self.film_blade.T_gas_stag, 0.5 * stage_prof.D1_in, 0.5 * stage_prof.D1_out)[0] / \
+                    (stage_prof.D1_out - stage_prof.D1_in)
+
+        T_gas_int = 0
+        for T, h in zip(self.film_blade.T_gas_stag_arr, self.film_blade.sector_height_arr):
+            T_gas_int += T * h
+
+        T_gas_av2 = 2 * T_gas_int / (stage_prof.D1_out - stage_prof.D1_in)
+
+        self.assertAlmostEqual(abs(T_gas_av1 - T_gas_av2) / T_gas_av1, 0, places=3)
+        self.assertAlmostEqual(abs(T_gas_av1 - self.turbine[0].T0_stag) / T_gas_av1, 0, places=3)
 
         self.assertEqual(sum(self.film_blade.sector_height_arr), 0.5 * (self.film_blade.D_out - self.film_blade.D_in))
         self.assertEqual(self.film_blade.sector_height_arr[0] * 2, self.film_blade.sector_height_arr[1])
         self.assertEqual(self.film_blade.sector_height_arr[2] * 2, self.film_blade.sector_height_arr[1])
-
-        self.film_blade.plot_lam_gas_out()
 
     def test_calculation(self):
         self.film_blade.compute()
@@ -324,7 +351,38 @@ class DeflectorBladeFilmCoolingTest(unittest.TestCase):
         self.film_blade.sectors[2].local_param.plot_all()
 
         self.film_blade.sectors[0].plot_v_gas()
-        self.film_blade.plot_T_wall()
+        self.film_blade.plot_T_wall(T_material=1000)
+
+    def test_profiling_results_for_cooling(self):
+        res = self.turbine_profiler[0].get_results_for_cooling('sa')
+        cooler = get_sa_cooler(
+            res,
+            self.film_blade.channel_width,
+            self.film_blade.wall_thickness,
+            self.film_blade.T_wall_out_av_init,
+            self.film_blade.lam_blade,
+            self.film_blade.x_hole_rel,
+            self.film_blade.hole_num,
+            self.film_blade.d_hole,
+            self.film_blade.phi_hole,
+            self.film_blade.mu_hole,
+            self.film_blade.work_fluid,
+            self.film_blade.T_cool0,
+            self.film_blade.p_cool_stag0,
+            self.film_blade.g_cool0,
+            self.film_blade.cool_fluid,
+            self.film_blade.node_num,
+            self.film_blade.accuracy
+        )
+        r_arr = np.linspace(0.5 * self.film_blade.D_in, 0.5 * self.film_blade.D_out, 10)
+
+        self.assertEqual(cooler.c_p_gas_av, self.film_blade.c_p_gas_av)
+        self.assertEqual(cooler.D_in, self.film_blade.D_in)
+        self.assertEqual(cooler.D_out, self.film_blade.D_out)
+        self.assertEqual([cooler.lam_gas_in(r) for r in r_arr], [self.film_blade.lam_gas_in(r) for r in r_arr])
+        self.assertEqual([cooler.lam_gas_out(r) for r in r_arr], [self.film_blade.lam_gas_out(r) for r in r_arr])
+        self.assertEqual([cooler.T_gas_stag(r) for r in r_arr], [self.film_blade.T_gas_stag(r) for r in r_arr])
+        self.assertEqual([cooler.p_gas_stag(r) for r in r_arr], [self.film_blade.p_gas_stag(r) for r in r_arr])
 
 
 

core/cooling/tools.py
@@ -7,6 +7,26 @@ from scipy.optimize import fsolve
 from scipy.interpolate import interp1d
 
 
+def get_theta(r_in, r_out, r_max_rel, theta_max):
+    """Возвращает функцию нервномерности с заданным аксимальным значением неравномерности,
+    заданным положением масимума на заданном интервале."""
+    r_max = r_max_rel * (r_out - r_in) + r_in
+    a = 3 * (r_out - r_in - theta_max * (r_out - r_in)) / ((r_out - r_max) ** 3 - (r_in - r_max) ** 3)
+    return lambda r: a * (r - r_max) ** 2 + theta_max
+
+
+def get_T_gas(theta_gas, T_c_stag, T_gas_stag_av):
+    """Возвращает распределение температуры газа с заданной неравномерностью, температурой
+    после компрессора и среднемассовой температурой газа."""
+    return lambda r: T_c_stag + theta_gas(r) * (T_gas_stag_av - T_c_stag)
+
+
+def get_g_cool(theta_g, g_cool_av):
+    """Возвращает распределение плотности расхода охлаждающего воздуха по радиусу с заданной
+    неравномернсотью и средним значение плотности расхода."""
+    return lambda r: g_cool_av * theta_g(r)
+
+
 class GasBladeHeatExchange:
 
     @classmethod
@@ -307,19 +327,23 @@ class LocalParamCalculator:
     def get_T_cool(self, x):
         return self._T_cool_int(x).__float__()
 
-    def plot_T_wall(self, figsize=(8, 6)):
+    def plot_T_wall(self, T_material_max, figsize=(8, 6), filename=None):
         plt.figure(figsize=figsize)
         plt.plot(self.x_arr * 1e3, self.T_wall_arr, lw=2, color='red')
         plt.xlim(min(self.x_arr) * 1e3, max(self.x_arr) * 1e3)
+        plt.plot([min(self.x_arr) * 1e3, max(self.x_arr) * 1e3],
+                 [T_material_max, T_material_max], lw=2, linestyle='--', color='black')
         T_max = max(self.T_wall_arr)
         plt.text(0.7 * min(self.x_arr) * 1e3, T_max - 100, r'$спинка$', fontsize=16)
         plt.text(0.3 * max(self.x_arr) * 1e3, T_max - 100, r'$корыто$', fontsize=16)
         plt.xlabel(r'$x,\ мм$', fontsize=14)
         plt.ylabel(r'$T_{ст},\ К$', fontsize=14)
         plt.grid()
+        if filename:
+            plt.savefig(filename)
         plt.show()
 
-    def plot_T_cool(self, figsize=(8, 6)):
+    def plot_T_cool(self, figsize=(8, 6), filename=None):
         plt.figure(figsize=figsize)
         plt.plot(self.x_arr * 1e3, self.T_cool_fluid_arr, lw=2, color='red')
         plt.xlim(min(self.x_arr) * 1e3, max(self.x_arr) * 1e3)
@@ -329,9 +353,11 @@ class LocalParamCalculator:
         plt.xlabel(r'$x,\ мм$', fontsize=14)
         plt.ylabel(r'$T_{в},\ К$', fontsize=14)
         plt.grid()
+        if filename:
+            plt.savefig(filename)
         plt.show()
 
-    def plot_T_out(self, figsize=(8, 6)):
+    def plot_T_out(self, figsize=(8, 6), filename=None):
         plt.figure(figsize=figsize)
         T_arr = [self.T_out_stag(x) for x in self.x_arr]
         plt.plot(self.x_arr * 1e3, T_arr, lw=2, color='red')
@@ -342,9 +368,11 @@ class LocalParamCalculator:
         plt.xlabel(r'$x,\ мм$', fontsize=14)
         plt.ylabel(r'$T_{обт.среды}^*,\ К$', fontsize=14)
         plt.grid()
+        if filename:
+            plt.savefig(filename)
         plt.show()
 
-    def plot_all(self, figsize=(8, 6)):
+    def plot_all(self, figsize=(8, 6), filename=None):
         plt.figure(figsize=figsize)
         T_arr = [self.T_out_stag(x) for x in self.x_arr]
         plt.plot(self.x_arr * 1e3, T_arr, lw=2, color='red', label=r'$T_{обт.среды}^*$')
@@ -356,8 +384,10 @@ class LocalParamCalculator:
         plt.text(0.3 * max(self.x_arr) * 1e3, T_max - 100, r'$корыто$', fontsize=16)
         plt.xlabel(r'$x,\ мм$', fontsize=14)
         plt.ylabel(r'$T,\ К$', fontsize=14)
-        plt.legend(fontsize=10)
+        plt.legend(fontsize=14)
         plt.grid()
+        if filename:
+            plt.savefig(filename)
         plt.show()
 
 
@@ -403,8 +433,11 @@ class FilmCalculator:
         self.x_hole = np.array(x_hole)
         self.hole_num = np.array(hole_num)
         self.d_hole = np.array(d_hole)
-        self.phi_hole = phi_hole
-        self.mu_hole = mu_hole
+        self.phi_hole = np.array(phi_hole)
+        self.mu_hole = np.array(mu_hole)
+
+        self._modify_hole_data()
+
         self.T_gas_stag = T_gas_stag
         self.p_gas_stag = p_gas_stag
         self.v_gas = v_gas
@@ -451,6 +484,68 @@ class FilmCalculator:
         self.film_eff_list = []
         "Список функций эффективности пленок от каждого ряда отверстий"
 
+    def _modify_hole_data(self):
+        """Создает массив, в который сохранена принадлежность отверстий к корыту или спинке. Также в массиве
+        координат отверстий дубоируется отверстие находящееся в начале координат. Соответствующим образом изменяются
+        массивы остальных параметров отверстий."""
+        zero_hole_num = list(self.x_hole).count(0)
+
+        x_hole = []
+        hole_num = []
+        d_hole = []
+        mu_hole = []
+        phi_hole = []
+        belong = []
+
+        if zero_hole_num == 0:
+            x_hole = list(self.x_hole)
+            hole_num = list(self.hole_num)
+            d_hole = list(self.d_hole)
+            mu_hole = list(self.mu_hole)
+            phi_hole = list(self.phi_hole)
+            for x in self.x_hole:
+                if x < 0:
+                    belong.append('s')
+                else:
+                    belong.append('k')
+
+        elif zero_hole_num == 1:
+            for i, x in enumerate(self.x_hole):
+                if x < 0:
+                    x_hole.append(x)
+                    hole_num.append(self.hole_num[i])
+                    d_hole.append(self.d_hole[i])
+                    mu_hole.append(self.mu_hole[i])
+                    phi_hole.append(self.phi_hole[i])
+                    belong.append('s')
+                elif x == 0:
+                    x_hole.append(0)
+                    x_hole.append(0)
+                    hole_num.append(self.hole_num[i] / 2)
+                    hole_num.append(self.hole_num[i] / 2)
+                    d_hole.append(self.d_hole[i])
+                    d_hole.append(self.d_hole[i])
+                    mu_hole.append(self.mu_hole[i])
+                    mu_hole.append(self.mu_hole[i])
+                    phi_hole.append(self.phi_hole[i])
+                    phi_hole.append(self.phi_hole[i])
+                    belong.append('s')
+                    belong.append('k')
+                elif x >= 0:
+                    x_hole.append(x)
+                    hole_num.append(self.hole_num[i])
+                    d_hole.append(self.d_hole[i])
+                    mu_hole.append(self.mu_hole[i])
+                    phi_hole.append(self.phi_hole[i])
+                    belong.append('k')
+
+        self.x_hole = np.array(x_hole)
+        self.d_hole = np.array(d_hole)
+        self.hole_num = np.array(hole_num)
+        self.mu_hole = np.array(mu_hole)
+        self.phi_hole = np.array(phi_hole)
+        self.belong = belong
+
     @classmethod
     def get_film_eff_func(cls, args):
         Re_s = args[0]
@@ -458,9 +553,10 @@ class FilmCalculator:
         phi = args[2]
         s = args[3]
         x_hole = args[4]
+        belong = args[5]
 
         def A(x):
-            if x_hole >= 0:
+            if belong == 'k':
                 res = Re_s ** (-0.25) * m ** (-1.3) * phi ** (-1.25) * (x - x_hole) / s
             else:
                 res = Re_s ** (-0.25) * m ** (-1.3) * phi ** (-1.25) * (x_hole - x) / s
@@ -523,7 +619,7 @@ class FilmCalculator:
                                     ))
 
         self.film_eff_list = list(map(self.get_film_eff_func,
-                                      zip(self.Re_s, self.m, self.phi_temp, self.s, self.x_hole)))
+                                      zip(self.Re_s, self.m, self.phi_temp, self.s, self.x_hole, self.belong)))
 
     def get_T_film(self, x):
         term1 = 1
@@ -540,18 +636,26 @@ class FilmCalculator:
         res = self.T_gas_stag * term1 + term2
         return res
 
-    def get_alpha_item(self, i, x):
-        if x != self.x_hole[i]:
-            if x >= 0:
-                return self.alpha_gas(x) * (1 + 2 * self.m[i] * self.s[i] / (x - self.x_hole[i]))
+    def _get_alpha_film(self, m, s, dx, x):
+        alpha_rel_max = 15
+        if dx != 0:
+            alpha_rel = (1 + 2 * m * s / dx)
+            if alpha_rel <= alpha_rel_max:
+                return alpha_rel * self.alpha_gas(x)
             else:
-                return self.alpha_gas(x) * (1 + 2 * self.m[i] * self.s[i] / (self.x_hole[i] - x))
+                return alpha_rel_max * self.alpha_gas(x)
         else:
-            return 0
+            return alpha_rel_max * self.alpha_gas(x)
+
+    def get_alpha_item(self, i, x):
+        if self.belong[i] == 'k':
+            return self._get_alpha_film(self.m[i], self.s[i], (x - self.x_hole[i]), x)
+        else:
+            return self._get_alpha_film(self.m[i], self.s[i], (self.x_hole[i] - x), x)
 
     def get_alpha_film(self, x):
-        x_hole_s = self.x_hole[self.x_hole < 0]
-        x_hole_k = self.x_hole[self.x_hole >= 0]
+        x_hole_s = self.x_hole[np.array([i == 's' for i in self.belong])]
+        x_hole_k = self.x_hole[np.array([i == 'k' for i in self.belong])]
 
         if x <= x_hole_s[0]:
             return self.get_alpha_item(0, x)
@@ -570,11 +674,11 @@ class FilmCalculator:
                     return self.get_alpha_item(i + len(x_hole_s), x)
 
     def get_G_cool(self, x):
-        x_hole_s = self.x_hole[self.x_hole < 0]
-        x_hole_k = self.x_hole[self.x_hole >= 0]
+        x_hole_s = self.x_hole[np.array([i == 's' for i in self.belong])]
+        x_hole_k = self.x_hole[np.array([i == 'k' for i in self.belong])]
 
-        dG_hole_s = self.dG_cool_hole[self.x_hole < 0]
-        dG_hole_k = self.dG_cool_hole[self.x_hole >= 0]
+        dG_hole_s = self.dG_cool_hole[np.array([i == 's' for i in self.belong])]
+        dG_hole_k = self.dG_cool_hole[np.array([i == 'k' for i in self.belong])]
 
         if x <= x_hole_s[0]:
             return self.G_cool0 - dG_hole_s.sum()
@@ -582,13 +686,17 @@ class FilmCalculator:
             return self.G_cool0 - dG_hole_k.sum()
         elif x_hole_s[len(x_hole_s) - 1] < x < x_hole_k[0]:
             return self.G_cool0
+        elif x == 0:
+            return self.G_cool0
 
         else:
             x_hole_s = list(x_hole_s)
             x_hole_s.reverse()
             x_hole_s = -1 * np.array(x_hole_s)
+            dG_hole_s = list(dG_hole_s)
+            dG_hole_s.reverse()
 
-            if x >= 0:
+            if x > 0:
                 dG = 0
                 for i in range(len(x_hole_k) - 1):
                     dG += dG_hole_k[i]
@@ -614,54 +722,101 @@ class FilmCalculator:
             min_value = round(min(value_arr), places) - delta
         return min_value, max_value
 
-    def plot_T_film(self, x_arr, places=-2, delta=50, figsize=(7, 5)):
+    def get_integrate_film_eff(self, x):
+        return (self.T_gas_stag - self.get_T_film(x)) / (self.T_gas_stag - self.T_cool(x))
+
+    def plot_integrate_film_eff(self, x_arr, figsize=(7, 5), filename=None):
+        plt.figure(figsize=figsize)
+        x_arr_new = [x * 1e3 for x in x_arr]
+        plt.plot(x_arr_new, [self.get_integrate_film_eff(x) for x in x_arr], lw=2, color='red')
+        plt.grid(True, axis='both')
+        plt.xlabel(r'$x,\ мм$', fontsize=14)
+        plt.ylabel(r'$\theta_{пл}$', fontsize=14)
+        plt.xlim(min(x_arr_new), max(x_arr_new))
+        if filename:
+            plt.savefig(filename)
+        plt.show()
+
+    def plot_T_film(self, x_arr, places=-2, delta=50, figsize=(7, 5), filename=None):
         plt.figure(figsize=figsize)
         T_arr = [self.get_T_film(x) for x in x_arr]
 
         T_min, T_max = self._get_lim(T_arr, places, delta)
-        plt.plot(x_arr, T_arr, lw=2)
+        x_arr_new = [x * 1e3 for x in x_arr]
+        plt.plot(x_arr_new, T_arr, lw=2)
         for x in self.x_hole:
-            plt.plot([x, x], [T_min, T_max], color='black', lw=1, linestyle='--')
+            plt.plot([x * 1e3, x * 1e3], [T_min, T_max], color='black', lw=1, linestyle='--')
         plt.grid()
         plt.ylim(T_min, T_max)
-        plt.xlabel(r'$x,\ м$', fontsize=14)
+        plt.xlim(min(x_arr_new), max(x_arr_new))
+        plt.xlabel(r'$x,\ мм$', fontsize=14)
         plt.ylabel(r'$T_{пл}^*,\ К$', fontsize=14)
+        if filename:
+            plt.savefig(filename)
         plt.show()
 
-    def plot_film_eff(self, hole_num, x_arr, create_fig=False, show=False, figsize=(9, 7)):
+    def plot_film_eff(self, hole_num, x_arr, create_fig=False, show=False, figsize=(9, 7), filename=None):
+        if self.x_hole[hole_num] == 0:
+            if hole_num > 0:
+                if self.x_hole[hole_num - 1] == 0:
+                    label = 'Отверстие %s' % hole_num
+                else:
+                    label = 'Отверстие %s' % (hole_num + 1)
+            else:
+                label = 'Отверстие %s' % (hole_num + 1)
+        else:
+            if list(self.x_hole).count(0) != 0:
+                if self.x_hole[hole_num] < 0:
+                    label = 'Отверстие %s' % (hole_num + 1)
+                else:
+                    label = 'Отверстие %s' % hole_num
+            else:
+                label = 'Отверстие %s' % (hole_num + 1)
         if create_fig:
             plt.figure(figsize=figsize)
-        plt.plot(x_arr, [self.film_eff_list[hole_num](x) for x in x_arr], lw=2.0, label='Отверстие %s' % (hole_num + 1))
+        x_arr_new = [x * 1e3 for x in x_arr]
+        plt.plot(x_arr_new, [self.film_eff_list[hole_num](x) for x in x_arr], lw=2.0, label=label)
         plt.legend(fontsize=10)
-        plt.xlabel(r'$x,\ м$', fontsize=14)
+        plt.xlabel(r'$x,\ мм$', fontsize=14)
         plt.ylabel(r'$\theta_{пл}$', fontsize=14)
         plt.grid(True, axis='both')
+        plt.xlim(min(x_arr_new), max(x_arr_new))
+        if filename:
+            plt.savefig(filename)
         if show:
             plt.show()
 
-    def plot_alpha_film(self, x_arr, ylim, figsize=(7, 5)):
+    def plot_alpha_film(self, x_arr, ylim, figsize=(7, 5), filename=None):
         plt.figure(figsize=figsize)
         alpha_arr = [self.get_alpha_film(x) for x in x_arr]
-        plt.plot(x_arr, alpha_arr, lw=2)
+        x_arr_new = [x * 1e3 for x in x_arr]
+        plt.plot(x_arr_new, alpha_arr, lw=2)
         alpha_min, alpha_max = ylim
         for x in self.x_hole:
-            plt.plot([x, x], [alpha_min, alpha_max], color='black', lw=1, linestyle='--')
+            plt.plot([x * 1e3, x * 1e3], [alpha_min, alpha_max], color='black', lw=1, linestyle='--')
         plt.grid()
         plt.ylim(alpha_min, alpha_max)
-        plt.xlabel(r'$x,\ м$', fontsize=14)
+        plt.xlim(min(x_arr_new), max(x_arr_new))
+        plt.xlabel(r'$x,\ мм$', fontsize=14)
         plt.ylabel(r'$\alpha_{пл},\ \frac{Вт}{м^2 \cdot К}$', fontsize=14)
+        if filename:
+            plt.savefig(filename)
         plt.show()
 
-    def plot_G_cool(self, x_arr, places=4, delta=1e-4, figsize=(7, 5)):
+    def plot_G_cool(self, x_arr, places=4, delta=1e-4, figsize=(7, 5), filename=None):
         plt.figure(figsize=figsize)
         G_arr = [self.get_G_cool(x) for x in x_arr]
-        plt.plot(x_arr, G_arr, lw=2)
+        x_arr_new = [x * 1e3 for x in x_arr]
+        plt.plot(x_arr_new, G_arr, lw=2)
 
         G_min, G_max = self._get_lim(G_arr, places, delta)
         for x in self.x_hole:
-            plt.plot([x, x], [G_min, G_max], color='black', lw=1, linestyle='--')
+            plt.plot([x * 1e3, x * 1e3], [G_min, G_max], color='black', lw=1, linestyle='--')
         plt.grid()
         plt.ylim(G_min, G_max)
-        plt.xlabel(r'$x,\ м$', fontsize=14)
+        plt.xlim(min(x_arr_new), max(x_arr_new))
+        plt.xlabel(r'$x,\ мм$', fontsize=14)
         plt.ylabel(r'$G_в,\ кг/с$', fontsize=14)
+        if filename:
+            plt.savefig(filename)
         plt.show()

core/profiling/section.py
@@ -84,14 +84,6 @@ class BladeSection:
         self.y_in_edge: np.ndarray = None
         self.x_out_edge: np.ndarray = None
         self.y_out_edge: np.ndarray = None
-        self.x_in_edge_s: np.ndarray = None
-        self.y_in_edge_s: np.ndarray = None
-        self.x_in_edge_k: np.ndarray = None
-        self.y_in_edge_k: np.ndarray = None
-        self.x_out_edge_s: np.ndarray = None
-        self.y_out_edge_s: np.ndarray = None
-        self.x_out_edge_k: np.ndarray = None
-        self.y_out_edge_k: np.ndarray = None
 
         # координаты центров окружностей скруглений на кромках
         self.x01: float = None
@@ -488,14 +480,7 @@ class BladeSection:
         self.y_in_edge += dy
         self.x_out_edge += dx
         self.y_out_edge += dy
-        self.x_in_edge_s += dx
-        self.y_in_edge_s += dy
-        self.x_in_edge_k += dx
-        self.y_in_edge_k += dy
-        self.x_out_edge_s += dx
-        self.y_out_edge_s += dy
-        self.x_out_edge_k += dx
-        self.y_out_edge_k += dy
+
         self.x_av += dx
         self.y_av += dy
 
@@ -642,8 +627,8 @@ class BladeSection:
         else:
             phi2 = np.arctan((self.y0_k - self.y01) / (self.x0_k - self.x01)) + np.pi
         phi = np.linspace(phi1, phi2, int(self.pnt_count / 2))
-        self.x_in_edge = np.array(self.x01 + self.r1 * np.cos(phi))
-        self.y_in_edge = np.array(self.y01 + self.r1 * np.sin(phi))
+        self.x_in_edge = self.x01 + self.r1 * np.cos(phi)
+        self.y_in_edge = self.y01 + self.r1 * np.sin(phi)
         self.length_in_edge = self._get_circle_arc_length(phi1, phi2, self.r1)
 
         self.x0, self.y0 = self._get_zero_point(self.angle1, self.x01, self.y01, self.r1)
@@ -655,8 +640,8 @@ class BladeSection:
         phi1 = np.arctan((self.y2_k - self.y02) / (self.x2_k - self.x02)) + np.pi
         phi2 = np.arctan((self.y2_s - self.y02) / (self.x2_s - self.x02)) + 2 * np.pi
         phi = np.linspace(phi1, phi2, int(self.pnt_count / 2))
-        self.x_out_edge = np.array(self.x02 + self.s2 * np.cos(phi))
-        self.y_out_edge = np.array(self.y02 + self.s2 * np.sin(phi))
+        self.x_out_edge = self.x02 + self.s2 * np.cos(phi)
+        self.y_out_edge = self.y02 + self.s2 * np.sin(phi)
         self.length_out_edge = self._get_circle_arc_length(phi1, phi2, self.s2)
 
         self.y_c, self.square_y = self._get_y_c(self.x_k, self.y_k, self.x_s, self.y_s, self.x_in_edge,
@@ -669,14 +654,14 @@ class BladeSection:
         self.length_k = self._get_arc_length(self.x_k, self.y_k)
         self.length_s = self._get_arc_length(self.x_s, self.y_s)
 
-        self.x_in_edge_s, self.x_in_edge_k = np.split(self.x_in_edge,
-                                                      [list(self.x_in_edge).index(self.x_in_edge.min())])
-        self.y_in_edge_s, self.y_in_edge_k = np.split(self.y_in_edge,
-                                                      [list(self.x_in_edge).index(self.x_in_edge.min())])
-        self.x_out_edge_k, self.x_out_edge_s = np.split(self.x_out_edge,
-                                                        [list(self.x_out_edge).index(self.x_out_edge.max())])
-        self.y_out_edge_k, self.y_out_edge_s = np.split(self.y_out_edge,
-                                                        [list(self.x_out_edge).index(self.x_out_edge.max())])
+        # self.x_in_edge_s, self.x_in_edge_k = np.split(self.x_in_edge,
+        #                                               [list(self.x_in_edge).index(self.x_in_edge.min())])
+        # self.y_in_edge_s, self.y_in_edge_k = np.split(self.y_in_edge,
+        #                                               [list(self.x_in_edge).index(self.x_in_edge.min())])
+        # self.x_out_edge_k, self.x_out_edge_s = np.split(self.x_out_edge,
+        #                                                 [list(self.x_out_edge).index(self.x_out_edge.max())])
+        # self.y_out_edge_k, self.y_out_edge_s = np.split(self.y_out_edge,
+        #                                                 [list(self.x_out_edge).index(self.x_out_edge.max())])
 
         if self.convex == 'left':
             self.x1_av, self.y1_av, self.x2_av, self.y2_av = self.x1_av, -self.y1_av, self.x2_av, -self.y2_av
@@ -691,10 +676,7 @@ class BladeSection:
             self.y0 = -self.y0
             self.y_in_edge = -self.y_in_edge
             self.y_out_edge = -self.y_out_edge
-            self.y_in_edge_k = -self.y_in_edge_k
-            self.y_in_edge_s = -self.y_in_edge_s
-            self.y_out_edge_k = -self.y_out_edge_k
-            self.y_out_edge_s = -self.y_out_edge_s
+
             self.y_c = -self.y_c
         elif self.convex == 'right':
             pass

core/profiling/stage.py
@@ -1,6 +1,7 @@
 import enum
 import typing
 import numpy as np
+import pickle
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 from ..average_streamline.stage_geom import InvalidStageSizeValue
@@ -298,6 +299,38 @@ class StageParametersRadialDistribution:
             if filename:
                 plt.savefig('%s_%s' % (filename, n))
         plt.show()
+
+
+class ProfilingResultsForCooling:
+    """Содержит результаты профилирования, необходимые для расчета конвективно-пленочного охлаждения."""
+    def __init__(
+            self, sections: typing.List[BladeSection],
+            D_in: float,
+            D_out: float,
+            c_p: float,
+            T_gas_stag: typing.List[float],
+            p_gas_stag: typing.List[float],
+            lam_gas_in: typing.List[float],
+            lam_gas_out: typing.List[float]
+    ):
+        """
+        :param sections: массив сечений.
+        :param D_in: внутренний диаметр.
+        :param D_out: наружний диаметр.
+        :param c_p: теплоемкость газа.
+        :param T_gas_stag: массив значений температур торможения газа по высоте лопатке.
+        :param p_gas_stag: массив значений давлений торможения газа по высоте лопатке.
+        :param lam_gas_in: массив значений приведенной скорости газа по высоте лопатке на входе в лопаточный венец.
+        :param lam_gas_out: массив значений приведенной скорости газа по высоте лопатке на входе из лопаточного венца.
+        """
+        self.sections = sections
+        self.D_in = D_in
+        self.D_out = D_out
+        self.c_p = c_p
+        self.T_gas_stag = T_gas_stag
+        self.p_gas_stag = p_gas_stag
+        self.lam_gas_in = lam_gas_in
+        self.lam_gas_out = lam_gas_out
 
 
 class StageProfiler(StageParametersRadialDistribution):
@@ -697,7 +730,7 @@ class StageProfiler(StageParametersRadialDistribution):
         plt.legend(fontsize=10)
         plt.show()
 
-    def write_nx_exp_file(self, filename: str, sec_num:int, blade_type='sa'):
+    def write_nx_exp_file(self, filename: str, sec_num: int, blade_type='sa'):
 
         if blade_type == 'sa':
             section = self.sa_sections[sec_num]
@@ -711,19 +744,45 @@ class StageProfiler(StageParametersRadialDistribution):
         with open(filename, 'w') as file:
             lines = []
 
-            lines.append('[degrees]angle1_l=%s\n' % (np.degrees([section.angle1_l])[0]))
-            lines.append('[degrees]angle2_l=%s\n' % (np.degrees([section.angle2_l])[0]))
-            lines.append('[degrees]gamma1_s=%s\n' % (np.degrees([section.gamma1_s])[0]))
-            lines.append('[degrees]gamma1_k=%s\n' % (np.degrees([section.gamma1_k])[0]))
-            lines.append('[degrees]gamma2_s=%s\n' % (np.degrees([section.gamma2_s])[0]))
-            lines.append('[degrees]gamma2_k=%s\n' % (np.degrees([section.gamma2_k])[0]))
-            lines.append('[mm]r1=%s\n' % (section.r1 * 1e3))
-            lines.append('[mm]s2=%s\n' % (section.s2 * 1e3))
-            lines.append('central_pole_pos=%s\n' % section.center_point_pos)
-            lines.append('z=%s\n' % z)
+            lines.append('[degrees]angle1_l_%s%s=%s\n' % (blade_type, sec_num, np.degrees([section.angle1_l])[0]))
+            lines.append('[degrees]angle2_l_%s%s=%s\n' % (blade_type, sec_num, np.degrees([section.angle2_l])[0]))
+            lines.append('[degrees]gamma1_s_%s%s=%s\n' % (blade_type, sec_num, np.degrees([section.gamma1_s])[0]))
+            lines.append('[degrees]gamma1_k_%s%s=%s\n' % (blade_type, sec_num, np.degrees([section.gamma1_k])[0]))
+            lines.append('[degrees]gamma2_s_%s%s=%s\n' % (blade_type, sec_num, np.degrees([section.gamma2_s])[0]))
+            lines.append('[degrees]gamma2_k_%s%s=%s\n' % (blade_type, sec_num, np.degrees([section.gamma2_k])[0]))
+            lines.append('[mm]r1_%s%s=%s\n' % (blade_type, sec_num, section.r1 * 1e3))
+            lines.append('[mm]s2_%s%s=%s\n' % (blade_type, sec_num, section.s2 * 1e3))
+            lines.append('central_pole_pos_%s%s=%s\n' % (blade_type, sec_num, section.center_point_pos))
+            lines.append('z_%s%s=%s\n' % (blade_type, sec_num, z))
 
             file.writelines(lines)
 
+    def get_results_for_cooling(self, blade_type='sa', pnt_num: int = 100) -> ProfilingResultsForCooling:
+        r_arr = np.linspace(0.5 * self.D1_in, 0.5 * self.D1_out, pnt_num)
+        if blade_type == 'sa':
+            sections = self.sa_sections
+            lam_gas_in = [self.lam_c0(r) for r in r_arr]
+            lam_gas_out = [self.lam_c1(r) for r in r_arr]
+            p_gas_stag = [self.p0_stag(r) for r in r_arr]
+            T_gas_stag = [self.T0_stag(r) for r in r_arr]
+        elif blade_type == 'rk':
+            sections = self.rk_sections
+            lam_gas_in = [self.lam_w1(r) for r in r_arr]
+            lam_gas_out = [self.lam_w2(r) for r in r_arr]
+            p_gas_stag = [self.p1_w_stag(r) for r in r_arr]
+            T_gas_stag = [self.T1_w_stag(r) for r in r_arr]
+        else:
+            raise ValueError("blade_type can not be equal to %s" % blade_type)
+
+        results = ProfilingResultsForCooling(sections, self.D1_in, self.D1_out, self.c_p,
+                                             T_gas_stag, p_gas_stag, lam_gas_in, lam_gas_out)
+        return results
+
+    def save_results_for_cooling(self, filename, blade_type='sa', pnt_num: int = 100):
+        results = self.get_results_for_cooling(blade_type, pnt_num)
+
+        with open(filename, 'wb') as file:
+            pickle.dump(results, file)
 
 if __name__ == '__main__':
     pass

gui/average_streamline/test.py
@@ -156,6 +156,7 @@ class AveLineWidgetTest(unittest.TestCase):
                           T_g_stag=1450,
                           p_g_stag=400e3,
                           G_turbine=25,
+                          G_fuel=1,
                           work_fluid=NaturalGasCombustionProducts(),
                           alpha_air=2.6,
                           l1_D1_ratio=0.20,
@@ -205,7 +206,7 @@ class AveLineWidgetTest(unittest.TestCase):
         self.assertAlmostEqual(self.form.T_g_stag.value(), turbine.T_g_stag, 2)
         self.assertAlmostEqual(self.form.p_g_stag.value(), turbine.p_g_stag / 1e6, places=3)
         self.assertAlmostEqual(self.form.G_t.value(), turbine.G_turbine, places=2)
-        self.assertAlmostEqual(self.form.alpha_air.value(), turbine.alpha_air, places=2)
+        self.assertAlmostEqual(self.form.G_fuel.value(), turbine.G_fuel, places=2)
         self.assertAlmostEqual(self.form.alpha11.value(), np.degrees(turbine.alpha11), places=1)
         self.assertAlmostEqual(self.form.l1_D1_ratio.value(), turbine.l1_D1_ratio, places=2)
         self.assertAlmostEqual(self.form.n.value(), turbine.n, places=0)